9d533be5daaa7a4e1c6c2be9d0b1175dced79bbf
Pquiachon <johnpaulquiachon2904@gmail.com>
2024-01-19 14:47:30 +0800

    refining the express file

--- app.js
+++ app.js
@@ -8,48 +8,10 @@ const app = ex();
 app.use(ex.json());
 app.use(cors());
 
-const e = exer.exercises;
+const e = exer.exe;
 // API
 app.get("/api/exercises", (req, res) => {
   res.send(e);
-});
-// main website
-app.get("/home", (req, res) => {
-  console.log("Someone accessed the home page");
-  fs.readFile("index.html", function (err, data) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    res.send();
-  });
-});
-
-app.get("/exercises", (req, res) => {
-  console.log("Someone accessed the home page");
-  fs.readFile("exercises.html", function (err, data) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    res.send();
-
-    console.log(e);
-  });
-});
-
-app.get("/instructors", (req, res) => {
-  console.log("Someone accessed the home page");
-  fs.readFile("instructors.html", function (err, data) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    res.send();
-  });
-});
-
-app.get("/about", (req, res) => {
-  console.log("Someone accessed the home page");
-  fs.readFile("about.html", function (err, data) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    res.send();
-  });
 });
 
 let port = process.env.PORT || 3000;
